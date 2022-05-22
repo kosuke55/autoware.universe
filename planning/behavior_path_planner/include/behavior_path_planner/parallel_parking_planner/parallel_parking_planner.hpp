49d71787c610824b4e581be96a6c2ec3c6396f5e
kosuke55 <kosuke.tnp@gmail.com>
2022-05-22 18:49:09 +0900

    Fix big arc and short straight path bug
    Signed-off-by: kosuke55 <kosuke.tnp@gmail.com>

--- planning/behavior_path_planner/include/behavior_path_planner/parallel_parking_planner/parallel_parking_planner.hpp
+++ planning/behavior_path_planner/include/behavior_path_planner/parallel_parking_planner/parallel_parking_planner.hpp
@@ -102,9 +102,7 @@ private:
   Pose getStartPose(
     const Pose goal_pose, const double start_pose_offset, const double R_E_r,
     const bool is_forward);
-  void getStraightPath(
-    const Pose goal_pose, const double start_pose_offset, const double R_E_r,
-    const bool is_forward);
+  void generateStraightPath(const Pose start_pose);
 
   // debug data
   PoseStamped Cr_;
